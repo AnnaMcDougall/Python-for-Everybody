817c7a1d5965c835109b7e8b73472d53b72e96dd
AnnaMcDougall <47217549+AnnaMcDougall@users.noreply.github.com>
2019-01-31 23:00:37 +0100

    Reduce to one loop
    On the challenge of my mentor, I reduced the original code to just one loop.

--- 2019-01-31-numberguess.py
+++ 2019-01-31-numberguess.py
@@ -5,23 +5,17 @@ x = input("Guess a number between 0 and 100: ")
 while isinstance(x, int) == False:
     try:
         x = int(x)
+        if x != n:
+            if x < 0:
+                print("WOMP WOMP: Invalid number!")
+            elif x > 100:
+                print("WOMP WOMP: Invalid number!")
+            elif x > n:
+                print("Too high")
+            elif x < n:
+                print("Too low!")
+            x = input("Try again: ")
+        else:
+          print("YES! You got it!")
     except:
         x = input("That is not a number, man! Guess again: ")
-
-while x != n:
-    if x < 0:
-        print("WOMP WOMP: Invalid number!")
-    elif x > 100:
-        print("WOMP WOMP: Invalid number!")
-    elif x > n:
-        print("Too high")
-    elif x < n:
-        print("Too low!")
-    x = input("Try again: ")
-    while isinstance(x, int) == False:
-        try:
-            x = int(x)
-        except:
-            x = input("That is not a number, man! Guess again: ")
-
-print("YES! You got it!")
